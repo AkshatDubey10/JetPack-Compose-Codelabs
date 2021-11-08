fb478d4d014d5004896c24d3bca431b80bbdd6ae
Akshat Dubey <akshatdubey2015@gmail.com>
2021-11-08 09:29:52 +0530

    Codelab completed

--- app/src/main/java/com/example/jetpackcomposecodelabs/ui/theme/Theme.kt
+++ app/src/main/java/com/example/jetpackcomposecodelabs/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorPalette = lightColors(
 @Composable
 fun JetPackComposeCodelabsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
         DarkColorPalette
